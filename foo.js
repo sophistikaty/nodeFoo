316cb52f30240df43476a16c84183567f552d75e
sophistikaty <sophistikaty@me.com>
2016-12-18 08:06:58 -0500

    fs & concurrency

--- foo.js
+++ foo.js
@@ -1,27 +1,43 @@
 
 var http = require( 'http' ),
+	fs = require('fs');
 
-	urls = ['shapeshed.com', 'www.bbc.co.uk', 'edition.cnn.com'];
+	var start = new Date();
 
-	function fetchPage(url){
-		var start = new Date();
-		http.get({ host:url}, function(response){
-			console.log('Got response from ' , url, response );
-			console.log('Request took ', new Date() - start, 'ms');
-		});
-	}
+	http.get({host:'nyt.com'}, function(response){
+		console.log('Got a response from nyt.com', response.statusCode);
+		console.log('Took ', new Date() - start, 'ms');
+	}).on('error', function(err){
+		console.log('there was an error', err);
+	});
 
-	for ( var i = 0; i < urls.length; i++ ){
-		fetchPage(urls[i]);
-	}
+	fs.readFile('file1.txt', 'utf8', function(err, data){
+		if(err) throw err;
+		console.log('file 1 read');
+		console.log('Took ', new Date() - start, 'ms');
+	});
+
+	http.get({host:'mashable.com'}, function(response){
+		console.log('Got a response from mashable.com', response.statusCode);
+		console.log('Took ', new Date() - start, 'ms');
+	}).on('error', function(err){
+		console.log('there was an error', err);
+	})
+
+	fs.readFile('file2.txt', 'utf8', function(err, data){
+		if(err) throw err;
+		console.log('file 2 read');
+		console.log('Took ', new Date() - start, 'ms');
+	});
 
 	function haveBreakfast(food,drink,callback){
 		console.log('Having breakfast of '+ food + ' ' + drink);
 		if(callback && typeof(callback == 'function')){
 			callback();
 		}
-	}
+	};
 
 	haveBreakfast('pancakes', 'americano', function(){
 		console.log('Finished breakfast, time to go to work');
-	})
+		console.log('Took ', new Date() - start, 'ms');
+	});
