5d408ff5a7dd2897894b2fbb528f72edb129945c
sophistikaty <sophistikaty@me.com>
2016-12-18 07:49:00 -0500

    responses

--- foo.js
+++ foo.js
@@ -6,7 +6,7 @@ var http = require( 'http' ),
 	function fetchPage(url){
 		var start = new Date();
 		http.get({ host:url}, function(response){
-			console.log('Got response from ' , url );
+			console.log('Got response from ' , url, response );
 			console.log('Request took ', new Date() - start, 'ms');
 		});
 	}
@@ -14,3 +14,14 @@ var http = require( 'http' ),
 	for ( var i = 0; i < urls.length; i++ ){
 		fetchPage(urls[i]);
 	}
+
+	function haveBreakfast(food,drink,callback){
+		console.log('Having breakfast of '+ food + ' ' + drink);
+		if(callback && typeof(callback == 'function')){
+			callback();
+		}
+	}
+
+	haveBreakfast('pancakes', 'americano', function(){
+		console.log('Finished breakfast, time to go to work');
+	})
