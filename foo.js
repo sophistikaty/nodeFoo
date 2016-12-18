a474504914d2726164eb1462b2bf39952004c48c
sophistikaty <sophistikaty@me.com>
2016-12-18 08:18:35 -0500

    blocking code

--- foo.js
+++ foo.js
@@ -2,33 +2,39 @@
 var http = require( 'http' ),
 	fs = require('fs');
 
-	var start = new Date();
+	fetchPage();
+	fetchApi();
 
-	http.get({host:'nyt.com'}, function(response){
-		console.log('Got a response from nyt.com', response.statusCode);
-		console.log('Took ', new Date() - start, 'ms');
-	}).on('error', function(err){
-		console.log('there was an error', err);
-	});
+	// http.get({host:'nyt.com'}, function(response){
+	// 	console.log('Got a response from nyt.com', response.statusCode);
+	// 	console.log('Took ', new Date() - start, 'ms');
+	// }).on('error', function(err){
+	// 	console.log('there was an error', err);
+	// });
 
-	fs.readFile('file1.txt', 'utf8', function(err, data){
-		if(err) throw err;
-		console.log('file 1 read');
-		console.log('Took ', new Date() - start, 'ms');
-	});
+	// fs.readFile('file1.txt', 'utf8', function(err, data){
+	// 	if(err) throw err;
+	// 	console.log('file 1 read');
+	// 	console.log('Took ', new Date() - start, 'ms');
+	// });
 
-	http.get({host:'mashable.com'}, function(response){
-		console.log('Got a response from mashable.com', response.statusCode);
-		console.log('Took ', new Date() - start, 'ms');
-	}).on('error', function(err){
-		console.log('there was an error', err);
-	})
+	// http.get({host:'mashable.com'}, function(response){
+	// 	console.log('Got a response from mashable.com', response.statusCode);
+	// 	console.log('Took ', new Date() - start, 'ms');
+	// }).on('error', function(err){
+	// 	console.log('there was an error', err);
+	// })
 
-	fs.readFile('file2.txt', 'utf8', function(err, data){
-		if(err) throw err;
-		console.log('file 2 read');
-		console.log('Took ', new Date() - start, 'ms');
-	});
+	// fs.readFile('file2.txt', 'utf8', function(err, data){
+	// 	if(err) throw err;
+	// 	console.log('file 2 read');
+	// 	console.log('Took ', new Date() - start, 'ms');
+	// });
+
+	// haveBreakfast('pancakes', 'americano', function(){
+	// 	console.log('Finished breakfast, time to go to work');
+	// 	console.log('Took ', new Date() - start, 'ms');
+	// });
 
 	function haveBreakfast(food,drink,callback){
 		console.log('Having breakfast of '+ food + ' ' + drink);
@@ -37,7 +43,21 @@ var http = require( 'http' ),
 		}
 	};
 
-	haveBreakfast('pancakes', 'americano', function(){
-		console.log('Finished breakfast, time to go to work');
-		console.log('Took ', new Date() - start, 'ms');
-	});
+	function sleep(milliseconds){
+		var start = new Date();
+		while((new Date().getTime() - start) < milliseconds){
+			//sleep
+		}
+	}
+
+	function fetchPage(){
+		console.log('fake fetching Page');
+		sleep(6000);
+		console.log('data returned from requesting page');
+	}
+
+	function fetchApi(){
+		console.log('fake fetching api');
+		sleep(4000);
+		console.log('data returned from api');
+	}
